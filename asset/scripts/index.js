ca2b7e9f34de54936dccccee754887b469bccd73
PENN JR <pennjunior680@gmail.com>
2022-12-16 15:00:13 +0100

    Doing some changes in my web calculator

--- asset/scripts/index.js
+++ asset/scripts/index.js
@@ -1,8 +1,4 @@
 // DOM Elements
-const hourEl = document.querySelector('.hour')
-const minuteEl = document.querySelector('.minute')
-const valueEl = document.querySelector('.value')
-
 const acEl = document.querySelector('.ac')
 const pmEl = document.querySelector('.pm')
 const percentEl = document.querySelector('.percent')
@@ -157,19 +153,3 @@ decimalEl.addEventListener('click', () => {
     setStrAsValue(currentValueStr + '.')
   }
 })
-
-// Set up the time
-const updateTime = () => {
-  const currentTime = new Date()
-
-  let currentHour = currentTime.getHours()
-  const currentMinute = currentTime.getMinutes()
-
-  if (currentHour > 12) {
-    currentHour -= 12
-  }
-  hourEl.textContent = currentHour.toString()
-  minuteEl.textContent = currentMinute.toString().padStart(2, '0')
-}
-setInterval(updateTime, 1000)
-updateTime()
